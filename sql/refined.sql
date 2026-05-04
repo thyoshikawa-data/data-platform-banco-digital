0ac08f80de7e004ddcdd9ae5a5906435e9b19fd4
thyoshikawa-data <thalesyoshi@gmail.com>
2026-05-04 19:22:24 -0300

    Update refined.sql

--- sql/refined.sql
+++ sql/refined.sql
@@ -4,7 +4,8 @@ SELECT
     TRUNC(data, 'MM') AS mes,
     AVG(valor) AS media_selic,
     MIN(valor) AS min_selic,
-    MAX(valor) AS max_selic
+    MAX(valor) AS max_selic,
+    COUNT(*) AS qtd_registros
 FROM TRUSTED_SELIC
 GROUP BY TRUNC(data, 'MM')
 ORDER BY mes;
