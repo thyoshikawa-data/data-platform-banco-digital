0b2c90cd1dab0990729714abab47c82886a68f35
thyoshikawa-data <thalesyoshi@gmail.com>
2026-05-06 13:50:51 -0300

    Create raw_market_trends.sql

---
+++ projeto-6-automotive-market-intelligence/sql/raw/raw_market_trends.sql
@@ -0,0 +1,7 @@
+sql id="agjlwm"
+CREATE TABLE RAW_MARKET_TRENDS (
+    trend_date DATE,
+    search_term VARCHAR2(100),
+    category VARCHAR2(50),
+    interest_score NUMBER,
+    load_date TIMESTAMP
